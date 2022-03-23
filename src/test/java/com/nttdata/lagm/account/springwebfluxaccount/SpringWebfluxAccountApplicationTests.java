a890681a219f0cdad4e7d041d2b96ebb705e6957
Jhon Ccamerccoa Perez <jccamerc@emeal.nttdata.com>
2022-03-23 16:26:03 -0500

    SonarQ

--- src/test/java/com/nttdata/lagm/account/springwebfluxaccount/SpringWebfluxAccountApplicationTests.java
+++ src/test/java/com/nttdata/lagm/account/springwebfluxaccount/SpringWebfluxAccountApplicationTests.java
@@ -3,7 +3,7 @@ package com.nttdata.lagm.account.springwebfluxaccount;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringWebfluxAccountApplicationTests.class)
 class SpringWebfluxAccountApplicationTests {
 
 	@Test
