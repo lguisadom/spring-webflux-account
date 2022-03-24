ae2818793ee1bc45e79906e960965991b0630d79
Luis Guisado <lguisadom@gmail.com>
2022-03-24 03:44:26 -0500

    Revert "SonarQ"
    This reverts commit a890681a219f0cdad4e7d041d2b96ebb705e6957.

--- src/test/java/com/nttdata/lagm/account/springwebfluxaccount/SpringWebfluxAccountApplicationTests.java
+++ src/test/java/com/nttdata/lagm/account/springwebfluxaccount/SpringWebfluxAccountApplicationTests.java
@@ -3,7 +3,7 @@ package com.nttdata.lagm.account.springwebfluxaccount;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SpringWebfluxAccountApplicationTests.class)
+@SpringBootTest
 class SpringWebfluxAccountApplicationTests {
 
 	@Test
